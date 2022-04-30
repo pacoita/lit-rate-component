3b9b636de6b33c2e4809fc85d789729ac826bfd8
Francesco Leardini <francesco.leardini@mobi.ch>
2022-04-30 17:07:41 +0200

    Added Dispatch event

--- projects/showcase-web-app/src/app/app.component.ts
+++ projects/showcase-web-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'showcaseWebApp';
+
+  selectedValue = 0;
+
+  displaySelection(event: Event) {
+    this.selectedValue = (event as CustomEvent<number>).detail;
+  }
 }
